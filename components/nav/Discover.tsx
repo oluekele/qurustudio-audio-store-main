9778b7ddd8c5fa488165f1e174727a4a7ef5377f
OluEkele <ekeleolu1@gmail.com>
2024-02-22 16:28:09 +0100

    commit

--- components/nav/Discover.tsx
+++ components/nav/Discover.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Discover = () => {
   return (
-    <div className='flex flex-col  absolute top-[50px] left-[46.5%] rounded-md border-[1px] border-[#212121] shadow-lg  bg-[#212121] p-3 gap-2 text-[15px] z-50'>
+    <div className='flex flex-col  absolute top-[50px] left-[48.5%] rounded-md border-[1px] border-[#212121] shadow-lg  bg-[#212121] p-3 gap-2 text-[15px] z-50'>
             <Link href={"/"} className='transition hover:text-[#ffa200] font-bold'>
               Treading Songs
             </Link>
